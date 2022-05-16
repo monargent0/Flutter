006c7bdcf990ad5964d43f796bc2cea193a3c405
jungeun <100196951+monargent0@users.noreply.github.com>
2022-05-16 18:24:29 +0900

    delete

--- text02_app/android/app/src/main/kotlin/com/example/text02_app/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.text02_app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- text_ex_app/android/app/src/main/kotlin/com/example/text_ex_app/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.text_ex_app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
